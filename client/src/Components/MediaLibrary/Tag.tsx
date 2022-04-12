a9360efdafe0b38bc33ded55e47b29d6a41692cb
peterclark1996@gmail.com <peterclark1996@gmail.com>
2022-04-12 22:37:54 +0100

    Improve tag styling

--- client/src/Components/MediaLibrary/Tag.tsx
+++ client/src/Components/MediaLibrary/Tag.tsx
@@ -9,15 +9,13 @@ type TagProps = {
 const Tag = ({ tag, onClick }: TagProps) => {
     return (
         <div
-            className={`d-flex align-items-center me-1 pointer ${classes.rounded} ${classes.shadow} ${classes.smallFont}`}
+            className={`d-flex align-items-center px-1 me-1 ${classes.rounded} ${classes.shadow} ${classes.smallFont}`}
             role="button"
             style={{ "backgroundColor": `#${tag.colourHex}` }}
             onClick={onClick}
         >
-            <span className="d-flex align-items-center ps-1 mb-1">{tag.name}</span>
-            <div className={`bg-secondary px-1 mx-1 ${classes.rounded}`}>
-                <i className="fa-solid fa-xmark" />
-            </div>
+            <span className="mb-1">{tag.name}</span>
+            <i className={`ms-1 text-light-grey fa-solid fa-circle-xmark`} />
         </div>
     )
 }
