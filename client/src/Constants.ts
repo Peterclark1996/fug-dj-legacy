170e9568a585ac6e7f012ebd0a3e2a0873ecb6d5
peterclark1996@gmail.com <peterclark1996@gmail.com>
2022-06-01 00:20:53 +0100

    Add prod urls to constants file

--- client/src/Constants.ts
+++ client/src/Constants.ts
@@ -25,12 +25,12 @@ export enum Resource {
 
 export enum ApiUrl {
     DEV = "https://localhost:7177/api/",
-    PROD = "https://localhost:7177/api/"
+    PROD = "https://fug-dj.herokuapp.com/api/"
 }
 
 export enum HubUrl {
     DEV = "https://localhost:7177/hub/room",
-    PROD = "https://localhost:7177/hub/room"
+    PROD = "https://fug-dj.herokuapp.com/hub/room"
 }
 
 export const getYoutubeUrl = (code: string) => `https://www.youtube.com/watch?v=${code}`
